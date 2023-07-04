c83dc9777dbf750f08467a90d4a85ba6c1119659
NagrjVV <nagaraj@aarsfs.com>
2023-07-04 15:18:47 +0530

    Delete Expense modal

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,5 @@
 export * from './bank-details.controller';
 export * from './cash-in-hand.controller';
-export * from './expenses.controller';
 export * from './transaction.controller';
 export * from './user.controller';
 
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,6 +1,5 @@
 export * from './bank-details.repository';
 export * from './cash-in-hand.repository';
-export * from './expenses.repository';
 export * from './transaction.repository';
 export * from './user.repository';
 
